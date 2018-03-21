531050cc8d2951e4bc44a769d3547c69f9653405
Mikita Kukavenka <nkukovenko@by.byelex.com>
2018-03-21 18:15:42 +0300

    added boneshaker

--- src/ScrollBearApp.js
+++ src/ScrollBearApp.js
@@ -92,5 +92,23 @@ const spellbook2 = [
       Any act by you or your apparent allies that threatens the commanded undead (regardless of its Intelligence) breaks the spell.
       Your commands are not telepathic. The undead creature must be able to hear you.`
     }
+  }, {
+    "title": "Boneshaker",
+    "spell": {
+      "title": "Boneshaker",
+      "school": "necromancy",
+      "level": "sorcerer/wizard 2",
+      "castingTime": "1 standard action",
+      "components": "V, S, F (human-shaped fetish made of bones)",
+      "range": "medium (100 ft. + 10 ft./level)",
+      "targets": "one living creature or undead creature with a skeleton",
+      "duration": "instantaneous",
+      "savingThrow": "Fortitude partial or negates (see text)",
+      "spellResistance": "yes",
+      "text": `By using a bone fetish like a marionette, you take control of a target creature’s skeleton. This has a variety of effects depending on whether the target is living or undead.
+A living creature has its skeleton rattle within its flesh, causing it grievous harm. The target takes 3d6 points of damage, plus 1d6 additional points of damage per 2 caster levels you have.
+In addition, you can move the target 5 feet. This movement doesn’t provoke attacks of opportunity. A successful saving throw halves the damage and negates the movement.
+An undead creature takes no damage. Instead, you manipulate the undead, forcing it to take an immediate action to either move up to its speed (provoking attacks of opportunity as normal) or make a single attack against a creature of your choice in its reach. Either of these is the most basic version of the action the creature can take (it doesn’t activate any special abilities that it could apply to the movement or attack, such as grab). A successful saving throw negates this effect. A mindless undead creature doesn’t receive a save against this effect.`
+    }
   }
 ];
