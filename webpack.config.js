d4a7baa4c88204032a3ba66f8b23810e9ce41fac
Mikita Kukavenka <nkukovenko@by.byelex.com>
2018-03-26 17:21:08 +0300

    Added webpack dev package

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
@@ -7,10 +8,16 @@ module.exports = {
     filename: 'scripts/app.js'
   },
   module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }]
-  }
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    })]
 };
