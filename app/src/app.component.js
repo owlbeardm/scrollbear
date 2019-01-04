80510f4254c1ae3c991fbe2e91e02d990d84e9ff
Mikita Kukavenka <nkukovenko@by.byelex.com>
2019-01-04 16:42:30 +0300

    added search

--- app/src/app.component.js
+++ app/src/app.component.js
@@ -1,4 +1,5 @@
 "use strict";
+const allSpells = require('../../resources/spells.json');
 
 function AppController($log, $scope, $window, focusService) {
   const ctrl = this;
@@ -6,7 +7,7 @@ function AppController($log, $scope, $window, focusService) {
 
   ctrl.$onInit = function() {
     $log.debug("AppController init");
-    ctrl.spells = require('../../resources/spells.json');
+    ctrl.spells = getSpells();
     $log.debug("AppController init", ctrl.spells);
     $log.debug("AppController init", ctrl.spells);
   }
@@ -14,6 +15,20 @@ function AppController($log, $scope, $window, focusService) {
   ctrl.chooseSpell = function(index) {
     ctrl.spell = ctrl.spells[index];
     $log.debug("chooseSpell", ctrl.spell);
+  }
+
+  ctrl.search = function() {
+    $log.debug("AppController ctrl.search", ctrl.filter);
+    ctrl.spells = getSpells();
+  }
+
+  function getSpells() {
+    return allSpells.filter((value) => {
+      if (!ctrl.filter){
+        return true;
+      }
+      return value.name.toLowerCase().includes(ctrl.filter.toLowerCase());
+    });
   }
 }
 
