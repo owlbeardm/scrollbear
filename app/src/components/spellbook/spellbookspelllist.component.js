a126bbfa5f9610b90a078ecadd76b3e564580d9c
c7d5a6 <godinfrog@gmail.com>
2019-02-09 20:36:03 +0300

    spellist for spellbook

--- app/src/components/spellbook/spellbookspelllist.component.js
+++ app/src/components/spellbook/spellbookspelllist.component.js
@@ -1,26 +1,45 @@
 "use strict";
 
-function SpellbookSpellListController($log, $state, filterService, spellService, CLASSES) {
+function SpellbookSpellListController($log, $state, $scope, notificationService, filterService, spellService, spellbookService, CLASSES) {
   $log.debug('SpellController create');
   const ctrl = this;
+  const SELECTED_CLASS = "SELECTED_CLASS";
 
   ctrl.$onInit = function() {
-    $log.debug("SpellListController init");
-    ctrl.classesC = CLASSES;
+    $log.debug("AppController init");
+    ctrl.classes = CLASSES;
+    ctrl.classSelected = spellbookService.selectedCharacter.class;
+    ctrl.setClass();
+    notificationService.subscribe($scope, notificationService.FILTER_CHANGED, (event, param) => {
+      ctrl.spells = getSpells();
+    });
   }
 
-  ctrl.chooseSpell = function(spell) {
-    ctrl.spell = spell;
-    const spell_url = spellService.spellNameToUrl(ctrl.spell.name);
-    $state.go('spells', {spellUrl: spell_url});
+  ctrl.search = function() {
+    $log.debug("AppController ctrl.search", ctrl.filter);
+    ctrl.spells = getSpells();
   }
 
-  ctrl.isFav = function(spell) {
-    return filterService.isFav(spell);
+  ctrl.setClass = function() {
+    spellService.setClass(ctrl.classSelected);
+    ctrl.search();
   }
 
-  ctrl.changeFav = function(spell) {
-    filterService.changeFav(spell);
+  ctrl.classToAll = function() {
+    ctrl.classSelected = 'all';
+    ctrl.setClass();
+  }
+
+  function getSpells() {
+    ctrl.otherSpellsCount = 0;
+    const allSells = spellService.getSpellsSplited();
+    ctrl.total = Object.entries(allSells).reduce(function(total, pair) {
+      return total + (pair[1].length);
+    }, 0);
+    if (!ctrl.total) {
+      ctrl.otherSpellsCount = spellService.getSpellsCountByFilter();
+    }
+    return allSells;
   }
 
 }
@@ -30,8 +49,11 @@ const SpellbookSpellListComponent = {
   controller: [
     '$log',
     '$state',
+    '$scope',
+    'notificationService',
     'filterService',
     'spellService',
+    'spellbookService',
     'CLASSES',
     SpellbookSpellListController
   ],
