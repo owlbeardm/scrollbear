ea9fd7eae621a745c297c11ea63c5b60a4c6fce2
Mikita Kukavenka <nkukovenko@by.byelex.com>
2019-03-12 18:37:31 +0300

    refactored components

--- app/src/components/spelllist/spelllist.module.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const spelllistComponents = angular.module('spelllist.components', []);
-
-import SpellListComponent from './spelllist.component.js';
-spelllistComponents.component('spelllist', SpellListComponent);
