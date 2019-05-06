2de8c22d0d89fe75bc583a9c3c69fbbe8940213c
c7d5a6 <godinfrog@gmail.com>
2019-05-06 22:53:16 +0300

    added history for spont

--- app/src/components/history/history.component.js
+++ app/src/components/history/history.component.js
@@ -1,21 +1,25 @@
 "use strict";
 
-function HistoryController($log, $state, spellbookService, CLASSES) {
+function HistoryController($log, $state, spellbookService) {
   $log.debug('HistoryController create');
   const ctrl = this;
 
   ctrl.$onInit = function() {
     $log.debug("HistoryController init");
-    ctrl.classes = CLASSES;
+  }
+
+  ctrl.changeHistory = function() {
+
+    ctrl.history = spellbookService.selectedCharacter.history;
+    $log.debug("HistoryController changeHistory", spellbookService.selectedCharacter);
   }
 
 }
 
 const HistoryComponent = {
   template: require('./history.html'),
-  controller: ['$log', '$state', 'spellbookService', 'CLASSES', HistoryController],
-  bindings: {
-  }
+  controller: ['$log', '$state', 'spellbookService', HistoryController],
+  bindings: {}
 }
 
 export default HistoryComponent;
