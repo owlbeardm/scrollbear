c87e627636208abca7ab0227bf81088968a398da
c7d5a6 <godinfrog@gmail.com>
2019-11-13 02:25:48 +0300

    vixed clean webpack plug

--- webpack.js
+++ webpack.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
